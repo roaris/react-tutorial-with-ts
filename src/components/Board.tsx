863058ffa77aa210f9caa21d73d5a31f36dbdc2b
roaris <one0803kyopro@gmail.com>
2022-06-05 23:57:58 +0900

    propsをGameコンポーネントに移動

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,58 +1,17 @@
-import { useState } from "react";
 import { Square } from "./Square";
 
-export const Board: React.FC = () => {
-  const [squares, setSquares] = useState<(string | null)[]>(
-    Array(9).fill(null)
-  );
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
+type BoardProps = {
+  squares: (string | null)[];
+  onClick: (i: number) => void;
+};
 
-  const handleClick = (i: number) => {
-    if (calculateWinner() || squares[i]) {
-      return;
-    }
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? "X" : "O";
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  };
-
+export const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
   const renderSquare = (i: number) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
-
-  const calculateWinner = () => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[b] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const winner = calculateWinner();
-  const status = winner
-    ? "Winner: " + winner
-    : "Next player: " + (xIsNext ? "X" : "O");
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,58 @@
+import { useState } from "react";
 import { Board } from "./Board";
 
 export const Game: React.FC = () => {
+  const [squares, setSquares] = useState<(string | null)[]>(
+    Array(9).fill(null)
+  );
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
+  const handleClick = (i: number) => {
+    if (calculateWinner() || squares[i]) {
+      return;
+    }
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
+  const calculateWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner();
+  const status = winner
+    ? "Winner: " + winner
+    : "Next player: " + (xIsNext ? "X" : "O");
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div></div>
+        <div className="status">{status}</div>
         <ol></ol>
       </div>
     </div>
